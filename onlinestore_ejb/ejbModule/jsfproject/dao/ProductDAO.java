9a24fc39b46dcca1c71b84f24af8d2af8edebc73
48503 <48503@LAPTOP-MFU454PS>
2021-01-11 23:41:21 +0100

    Session, roles

--- onlinestore_ejb/ejbModule/jsfproject/dao/ProductDAO.java
+++ onlinestore_ejb/ejbModule/jsfproject/dao/ProductDAO.java
@@ -6,7 +6,7 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-
+import javax.persistence.TypedQuery;
 
 import jsfproject.entities.Product;
 import jsfproject.entities.User;
@@ -14,10 +14,30 @@ import jsfproject.entities.User;
 @Stateless
 public class ProductDAO {
 	private final static String UNIT_NAME = "jsfproject-simplePU";
-	
+
 	@PersistenceContext(unitName = UNIT_NAME)
 	protected EntityManager em;
+
+	private Integer quantity = 2;
+	private Integer page;
+	private Integer lastPage;
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public Integer getPage() {
+		return page;
+	}
 	
+	public void setPage(int page) {
+		this.page = page;
+	}
+
+	public Integer getLastPage() {
+		return lastPage;
+	}
+
 	public void create(Product product) {
 		em.persist(product);
 	}
@@ -33,11 +53,20 @@ public class ProductDAO {
 	public Product find(Object id) {
 		return em.find(Product.class, id);
 	}
-	
+
+	public int countProducts() {
+		List<Product> list = em.createQuery("SELECT p FROM Product p", Product.class).getResultList();
+		return list.size();
+	}
+
 	public List<Product> listAllProducts() {
-        return em.createQuery("SELECT p FROM Product p", Product.class).getResultList();
-    }
-	
-	
+		lastPage = (int) Math.floor(countProducts() / quantity);
+
+		if (page == null || page > lastPage || page < 1)
+			page = 1;
+
+		return (List<Product>) em.createQuery("SELECT p FROM Product p", Product.class).setMaxResults(getQuantity())
+				.setFirstResult(getQuantity()).getResultList();
+	}
 
 }
